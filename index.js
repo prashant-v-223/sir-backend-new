d791ada29c41a19fc31845ab8fd8e2c9a44562c5
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2023-11-01 18:03:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -116,60 +116,82 @@ const updateRank = async (user, newRank, rewardAmount, teamtotalstack) => {
     Rank: user.Rank,
   });
   let data1 = await findAllRecord(Usermodal, {
-    supporterId: data.username
-
+    supporterId: data.username,
   });
+  console.log(
+    "data1.lengthdata1.lengthdata1.lengthdata1.lengthdata1.lengthdata1.length",
+    data1.length
+  );
   if (data1.length >= 5) {
-    await Usermodal.findByIdAndUpdate({ _id: user._id, }, {
-      STAKINGBOOSTER: true
-    })
-    data1.sort((a, b) => b.teamtotalstack - a.teamtotalstack);
-    let lastteamtotalstack = 0
-    const lastThreeObjects = data1.slice(-3);
-    for (let index = 0; index < lastThreeObjects.length; index++) {
-      lastteamtotalstack += lastThreeObjects[index].teamtotalstack;
-    }
-    console.log(teamtotalstack * 0.25);
-    console.log(lastteamtotalstack * 0.25);
-    if (teamtotalstack * 0.50 <= data1[0].teamtotalstack * 0.50) {
-      if (teamtotalstack * 0.25 <= data1[1].teamtotalstack * 0.25) {
-        let lastteamtotalstack = 0
-        const lastThreeObjects = data1.slice(-3);
-        for (let index = 0; index < lastThreeObjects.length; index++) {
-          lastteamtotalstack += lastThreeObjects[index].teamtotalstack;
-        }
-        if (teamtotalstack * 0.25 <= lastteamtotalstack * 0.25) {
-          await updateRecord(
-            Usermodal,
-            {
-              _id: user._id,
-              Rank: user.Rank,
-              teamtotalstack: { $gt: teamtotalstack },
-            },
-            { Rank: newRank }
-          );
-          const da = await findAllRecord(Usermodal, {
-            _id: user._id,
-            Rank: newRank,
-          });
-
-          if (da.length > 0) {
-            let data = {
-              userId: user._id,
-              Note: `You Have Acheicer New ${rewardAmount}`,
-              Amount: rewardAmount,
-            };
-            await Achivement(data).save();
+    await Usermodal.findByIdAndUpdate(
+      { _id: user._id },
+      {
+        STAKINGBOOSTER: true,
+      }
+    );
+    let data = await Stakingmodal.find({ userId: user._id });
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      let d = await Stakingmodal.find({ userId: element.userId, leval: 0 });
+      for (let index = 0; index < d.length; index++) {
+        const element123 = d[index];
+        let a = 500 - element123.Totalsend;
+        let b = element123.Amount / 1000 * 2;
+        await Stakingmodal.findOneAndUpdate(
+          { _id: element123._id },
+          {
+            DailyReword: b,
+            TotaldaysTosendReword: a,
           }
-        }
-
+        );
       }
     }
+
+    // data1.sort((a, b) => b.teamtotalstack - a.teamtotalstack);
+    // let lastteamtotalstack = 0;
+    // const lastThreeObjects = data1.slice(-3);
+    // for (let index = 0; index < lastThreeObjects.length; index++) {
+    //   lastteamtotalstack += lastThreeObjects[index].teamtotalstack;
+    // }
+    // lo
+    // if (teamtotalstack * 0.5 <= data1[0].teamtotalstack  * 0.5) {
+    //   if (teamtotalstack * 0.25 <= data1[1].teamtotalstack * 0.25) {
+    //     let lastteamtotalstack = 0;
+    //     const lastThreeObjects = data1.slice(-3);
+    //     for (let index = 0; index < lastThreeObjects.length; index++) {
+    //       lastteamtotalstack += lastThreeObjects[index].teamtotalstack;
+    //     }
+    //     if (teamtotalstack * 0.25 <= lastteamtotalstack * 0.25) {
+    //       await updateRecord(
+    //         Usermodal,
+    //         {
+    //           _id: user._id,
+    //           Rank: user.Rank,
+    //           teamtotalstack: { $gt: teamtotalstack },
+    //         },
+    //         { Rank: newRank }
+    //       );
+    //       const da = await findAllRecord(Usermodal, {
+    //         _id: user._id,
+    //         Rank: newRank,
+    //       });
+
+    //       if (da.length > 0) {
+    //         let data = {
+    //           userId: user._id,
+    //           Note: `You Have Acheicer New ${rewardAmount}`,
+    //           Amount: rewardAmount,
+    //         };
+    //         await Achivement(data).save();
+    //       }
+    //     }
+    //   }
+    // }
   }
 };
-schedule.scheduleJob("*/5 * * * *", async () => {
+schedule.scheduleJob("*/5 * * * * *", async () => {
   try {
-    const Userdata = await findAllRecord(Usermodal, {});
+    const Userdata = await findAllRecord(Usermodal, { username: "SIR79897" });
     for (const user of Userdata) {
       await Usermodal.aggregate([
         {
@@ -180,11 +202,11 @@ schedule.scheduleJob("*/5 * * * *", async () => {
       ]).then(async (res) => {
         if (res.length > 0) {
           switch (res[0]?.Rank) {
-            case "DIRECT":
-              await updateRank(res[0], "EXECUTIVE", "SMART WATCH", 1000);
+            case "Trainee":
+              await updateRank(res[0], "ACE", "SMART WATCH", 1000);
               break;
-            case "EXECUTIVE":
-              await updateRank(res[0], "SR. EXECUTIVE", "SMART PHONE", 7000);
+            case "ACE":
+              await updateRank(res[0], "WARRIOR", "SMART PHONE", 7000);
               break;
             case "SR. EXECUTIVE":
               await updateRank(res[0], "MANAGER", "INTERNATIONAL TRIP", 20000);
@@ -196,35 +218,40 @@ schedule.scheduleJob("*/5 * * * *", async () => {
               await updateRank(res[0], "PEARL", "WAGONR/$6000", 50000);
               break;
             case "PEARL":
-              await updateRank(
-                res[0],
-                "SAPPHIRE",
-                "BREEZA/$12500",
-                300000
-              );
+              await updateRank(res[0], "SAPPHIRE", "BREEZA/$12500", 300000);
               break;
             case "SAPPHIRE":
-              await updateRank(
-                res[0],
-                "RUBY",
-                "2BHK FLAT/$30000",
-                700000
-              );
+              await updateRank(res[0], "RUBY", "2BHK FLAT/$30000", 700000);
               break;
             case "RUBY":
               await updateRank(res[0], "DIAMOND", "MERCEDEZ/$48000", 1500000);
               break;
             case "DIAMOND":
-              await updateRank(res[0], "BLUE DIAMOND", "3/4 BHK APARTMENT/$100000", 3400000);
+              await updateRank(
+                res[0],
+                "BLUE DIAMOND",
+                "3/4 BHK APARTMENT/$100000",
+                3400000
+              );
               break;
             case "BLUE DIAMOND":
               await updateRank(res[0], "CROWN", "VILLA/ $300000", 7000000);
               break;
             case "CROWN":
-              await updateRank(res[0], "CROWN DIAMOND", "ROLLS ROYCE/ $400000", 15000000);
+              await updateRank(
+                res[0],
+                "CROWN DIAMOND",
+                "ROLLS ROYCE/ $400000",
+                15000000
+              );
               break;
             case "CROWN DIAMOND":
-              await updateRank(res[0], "KING", "PRIVATE JET/ $1000000", 30000000);
+              await updateRank(
+                res[0],
+                "KING",
+                "PRIVATE JET/ $1000000",
+                30000000
+              );
               break;
             default:
               break;
@@ -236,125 +263,6 @@ schedule.scheduleJob("*/5 * * * *", async () => {
     console.log(error);
   }
 });
-// schedule.scheduleJob("*/30 * * * * *", async () => {
-//   try {
-//     const Userdata = await findAllRecord(Usermodal, {});
-//     for (const user of Userdata) {
-//       await Usermodal.aggregate([
-//         {
-//           $match: {
-//             username: user.username,
-//           },
-//         },
-//       ]).then(async (res) => {
-//         if (res.length > 0) {
-//           switch (res[0]?.Rank) {
-//             case "DIRECT":
-//               const Refflevalncome = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "DIRECT",
-//               });
-//               if (Refflevalncome.length >= 4) {
-//                 console.log(Refflevalncome);
-//                 await updateRank(res[0], "EXECUTIVE", 50, 2500);
-//               }
-//               break;
-//             case "EXECUTIVE":
-//               const Refflevalncome1 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "EXECUTIVE",
-//               });
-//               if (Refflevalncome1.length >= 2) {
-//                 await updateRank(res[0], "Sales EXECUTIVE", 100, 10000);
-//               }
-//               break;
-//             case "Sales EXECUTIVE":
-//               const Refflevalncome2 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "Sales EXECUTIVE",
-//               });
-//               if (Refflevalncome2.length >= 2) {
-//                 await updateRank(res[0], "AREA SALES MANAGER", 250, 40000);
-//               }
-//               break;
-//             case "AREA SALES MANAGER":
-//               const Refflevalncome3 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "AREA SALES MANAGER",
-//               });
-//               if (Refflevalncome3.length >= 2) {
-//                 await updateRank(res[0], "Zonal HEAD", 500, 160000);
-//               }
-//               break;
-//             case "Zonal HEAD":
-//               const Refflevalncome4 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "Zonal HEAD",
-//               });
-//               if (Refflevalncome4.length >= 2) {
-//                 await updateRank(res[0], "PROJECT HEAD", 1500, 5000000);
-//               }
-//               break;
-//             case "PROJECT HEAD":
-//               const Refflevalncome5 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "PROJECT HEAD",
-//               });
-//               if (Refflevalncome5.length >= 2) {
-//                 await updateRank(
-//                   res[0],
-//                   "Sr. Project Head",
-//                   5000,
-//                   308480
-//                 );
-//               }
-//               break;
-//             case "Sr. Project Head":
-//               const Refflevalncome6 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "Sr. Project Head",
-//               });
-//               if (Refflevalncome6.length >= 2) {
-//                 await updateRank(res[0], "COM-B", 15000, 10000000);
-//               }
-//               break;
-//             case "COM-B":
-//               const Refflevalncome7 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "COM-B",
-//               });
-//               if (Refflevalncome7.length >= 2) {
-//                 await updateRank(res[0], "COM-A", 75000, 30000000);
-//               }
-//               break;
-//             case "COM-A":
-//               const Refflevalncome8 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "COM-A",
-//               });
-//               if (Refflevalncome8.length >= 2) {
-//                 await updateRank(res[0], "TRUST", 150000, 6000000);
-//               }
-//               break;
-//             case "TRUST":
-//               const Refflevalncome9 = await findAllRecord(Usermodal, {
-//                 refferalBy: res[0].username,
-//                 Rank: "TRUST",
-//               });
-//               if (Refflevalncome9.length >= 2) {
-//                 await updateRank(res[0], "CORE TEAM", 500000, 10000000);
-//               }
-//               break;
-//             default:
-//               break;
-//           }
-//         }
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
 schedule.scheduleJob("*/5 * * * *", async () => {
   try {
     const Userdata = await findAllRecord(Usermodal, {});
@@ -363,7 +271,7 @@ schedule.scheduleJob("*/5 * * * *", async () => {
       await Usermodal.aggregate([
         {
           $match: {
-            username
+            username,
           },
         },
         {
@@ -384,10 +292,10 @@ schedule.scheduleJob("*/5 * * * *", async () => {
             pipeline: [
               {
                 $match: {
-                  Active: true, WalletType: { $in: ["main-Wallet", "DAPP-WALLET"] }
-
-                }
-              }
+                  Active: true,
+                  WalletType: { $in: ["main-Wallet", "DAPP-WALLET"] },
+                },
+              },
             ],
           },
         },
@@ -400,10 +308,10 @@ schedule.scheduleJob("*/5 * * * *", async () => {
             pipeline: [
               {
                 $match: {
-                  Active: true, WalletType: { $in: ["main-Wallet", "DAPP-WALLET"] }
-
-                }
-              }
+                  Active: true,
+                  WalletType: { $in: ["main-Wallet", "DAPP-WALLET"] },
+                },
+              },
             ],
           },
         },
@@ -435,11 +343,14 @@ schedule.scheduleJob("*/5 * * * *", async () => {
       ]).then(async (aggregatedUserData) => {
         console.log(aggregatedUserData);
         if (aggregatedUserData.length > 0) {
-          let data1 = await Usermodal.find({ username: aggregatedUserData[0].username })
+          let data1 = await Usermodal.find({
+            username: aggregatedUserData[0].username,
+          });
           await Stakingmodal.updateMany(
-            { userId: data1[0]._id, leval: data1[0].leval }, {
-            Active: true
-          }
+            { userId: data1[0]._id, leval: data1[0].leval },
+            {
+              Active: true,
+            }
           );
           await Usermodal.findOneAndUpdate(
             { _id: ObjectId(userId) },
