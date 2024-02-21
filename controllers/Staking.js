8a71d35a2550fd8b305d21c92761e0ebdef85a9d
prashant-v-223 <prashantvadhvana@gmail.com>
2024-02-22 00:50:51 +0530

    asd

--- controllers/Staking.js
+++ controllers/Staking.js
@@ -119,10 +119,10 @@ const cronHandler = async (user) => {
   }
 }
 const nowIST = new Date();
-nowIST.setUTCHours(nowIST.getUTCHours() + 1, nowIST.getUTCMinutes(), 0, 0); // Convert to IST
+nowIST.setUTCHours(nowIST.getUTCHours(), nowIST.getUTCMinutes(), 0, 0); // Convert to IST
 
 const todayIST = new Date(nowIST);
-todayIST.setHours(0, 0, 0, 0);
+todayIST.setHours(0, 1, 0, 0);
 
 const nextDayIST = new Date(todayIST);
 nextDayIST.setDate(nextDayIST.getDate() + 1);
