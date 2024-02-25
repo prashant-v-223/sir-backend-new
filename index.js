af0f7cd828fef1f2703ba189e51f5f156920bf89
prashant-v-223 <prashantvadhvana@gmail.com>
2024-02-25 07:22:57 +0530

    asdsda

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ app.get('/show-image', (req, res) => {
   res.sendFile(imagePath);
 });
 
-const every24hours = "30 12 * * *";
-const every24hours1 = "30 13 * * *";
+const every24hours = "30 7 * * *";
+const every24hours1 = "40 7 * * *";
 schedule.scheduleJob(every24hours, async () => {
   try {
     const Userdata = await findAllRecord(Usermodal, {});
