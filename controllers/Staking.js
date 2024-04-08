a84851b725f3d9fcc33e9f0321cdab7f2bde8345
prashant-v-223 <prashantvadhvana@gmail.com>
2024-04-08 22:23:40 +0530

    asd

--- controllers/Staking.js
+++ controllers/Staking.js
@@ -1621,7 +1621,6 @@ exports.stack = {
                           {
                             $lt: ["$$item.createdAt", new Date(nextDayIST)],
                           },
-                          { $eq: [{ $substrCP: ["$$item.Note", 0, 28] }, "You Got Staking Bonus Income"] }
                         ],
                       },
                     },
