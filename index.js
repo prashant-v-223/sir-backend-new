416c4e973460677d4a4ca918f43b57035d5dd03d
prashant-v-223 <prashantvadhvana@gmail.com>
2024-05-10 17:12:14 +0530

    aD

--- index.js
+++ index.js
@@ -47,10 +47,11 @@ nextDayIST.setHours(0, 0, 0, 0);
 
 
 const nowIST1 = new Date();
-nowIST1.setUTCHours(nowIST1.getUTCHours(), nowIST1.getUTCMinutes(), 0, 0); // Convert to IST
+
+nowIST1.setUTCHours(nowIST1.getUTCHours() - 24 , nowIST1.getUTCMinutes(), 0, 0); // Convert to IST
 // Set time to midnight for the current day in IST
 const startOfDayIST = new Date(nowIST1);
-startOfDayIST.setHours(12, 0, 0, 0);
+startOfDayIST.setHours(0, 0, 0, 0);
 
 const endOfDayIST = new Date(nowIST1);
 endOfDayIST.setDate(endOfDayIST.getDate() + 1);
